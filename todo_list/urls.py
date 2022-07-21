80b52c527b833108f8d4b198e0a2b4e648d05ff5
avinash <avinashyadav145053@gmail.com>
2022-07-21 13:50:00 +0530

    altered urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('todo/',todolistview.as_view()),
+	path('',todolistview.as_view()),
 	path('tododetails/<str:pk>', tododetailview.as_view())
 ]
